a1777fcf9bdb859a5edd9ef4aeef68e0dea6292b
zylw516565 <zylw516565@163.com>
2026-01-08 23:18:23 +0800

    update  proofofwork 1

--- bootcamp/simulate_blockchain/proofofwork.go
+++ bootcamp/simulate_blockchain/proofofwork.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+	"math"
 	"math/big"
 )
 
 const targetBits = 24
+
+var (
+	maxNonce = math.MaxInt64
+)
 
 type ProofOfWork struct {
 	block  *Block
@@ -16,4 +24,41 @@ func NewProofOfWork(block *Block) *ProofOfWork {
 	target.Lsh(target, uint(256-targetBits))
 
 	return &ProofOfWork{block, target}
+}
+
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			IntToHex(pow.block.Timestamp),
+			pow.block.PrevBlockHash,
+			pow.block.Data,
+			IntToHex(int64(targetBits)),
+			IntToHex(int64(nonce)),
+		},
+		[]byte{},
+	)
+
+	return data
+}
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var hashInt big.Int
+	var hash [32]byte
+	nonce := 0
+	fmt.Printf("Mining the block containing \"%s\"\n", pow.block.Data)
+	for nonce < maxNonce {
+		data := pow.prepareData(nonce)
+		hash = sha256.Sum256(data)
+		hashInt.SetBytes(hash[:])
+
+		if hashInt.Cmp(pow.target) == -1 {
+			fmt.Printf("\r%x", hash)
+			break
+		} else {
+			nonce++
+		}
+	}
+	fmt.Print("\n\n")
+
+	return nonce, hash[:]
 }
